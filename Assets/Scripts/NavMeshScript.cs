2c2c0e1e6dc086c314222cb32e51d53d71c66d7e
LuanaNoira <a47523@alunos.ipb.pt>
2022-05-24 17:55:11 +0100

    AI slime teste cena

--- Assets/Scripts/NavMeshScript.cs
+++ Assets/Scripts/NavMeshScript.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshScript : MonoBehaviour
 {
-   // public Naveshagent
+    public NavMeshAgent agent;
+
+    public GameObject destination; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent.SetDestination(destination.transform.position);
     }
 
     // Update is called once per frame
