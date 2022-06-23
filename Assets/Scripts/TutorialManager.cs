969b6fa458802f436fb5d82063835535d0530e6d
Samuel47535 <a47535@alunos.ipb.pt>
2022-06-23 23:39:10 +0100

    aaa

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -5,8 +5,55 @@ using UnityEngine;
 public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
-    private int popUpIndex;
+    [SerializeField]private int popUpIndex;
+    [SerializeField] private SlimeTarget slimeTutorial;
 
+    private void Start()
+    {
+        slimeTutorial = slimeTutorial.GetComponent<SlimeTarget>();
+    }
+
+
+    private void Update()
+    {
+        for (int i = 0; i <  popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+
+        if(popUpIndex == 0)
+        {
+            popUps[popUpIndex].SetActive(true);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S))
+            {
+                popUpIndex++;
+            }
+        }
+        else if(popUpIndex == 1)
+        {
+            popUps[popUpIndex].SetActive(true);
+            popUps[popUpIndex - 1].SetActive(false);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                popUpIndex++;
+            }
+        }
+        else if(popUpIndex == 2)
+        {
+            popUps[popUpIndex - 1].SetActive(false);
+            if (slimeTutorial.charmed)
+                popUps[popUpIndex].SetActive(false);
+        }
+    }
+
+    /*
 
     private void Update()
     {
@@ -39,4 +86,6 @@ public class TutorialManager : MonoBehaviour
         else if (popUpIndex == 2)
         { }
     }
+
+    */
 }
